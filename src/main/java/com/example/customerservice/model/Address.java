0514d03ff4ab38ddb3bc29849e6217edc9950de4
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-08-16 16:27:11 +0200

    Enhance `Address` model: Add MongoDB annotations, `id`, `NoArgsConstructor` and `AllArgsConstructor` for better flexibility and persistence support.

--- src/main/java/com/example/customerservice/model/Address.java
+++ src/main/java/com/example/customerservice/model/Address.java
@@ -1,13 +1,27 @@
 package com.example.customerservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "addresses")
 public class Address {
-    private String street;
-    private String city;
-    private String state;
-    private String postalCode;
-    private String country;
+    @Id
+    private String      id;
+    private String      street;
+    private String      city;
+    private String      state;
+    private String      postalCode;
+    private String      country;
 
+    public void generateID() {
+        this.id = String.valueOf(UUID.randomUUID());
+    }
 }
