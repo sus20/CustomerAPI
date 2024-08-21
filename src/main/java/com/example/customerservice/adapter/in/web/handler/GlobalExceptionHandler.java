9ab15a57abc5b38f75a8cf4d9680c5d1aa91fc72
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-08-21 12:38:41 +0200

    Refactored GlobalExceptionHandler to improve exception handling. Added specific exception handlers for Bank and Address-related exceptions.

--- src/main/java/com/example/customerservice/adapter/in/web/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/customerservice/adapter/in/web/handler/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.example.customerservice.adapter.in.web.handler;
 
+import com.example.customerservice.core.exception.address.AddressAlreadyExistsException;
 import com.example.customerservice.core.exception.address.AddressIdMismatchException;
+import com.example.customerservice.core.exception.address.AddressNotBelongToCustomerException;
 import com.example.customerservice.core.exception.address.AddressNotFoundException;
+import com.example.customerservice.core.exception.bank.BankAccountAlreadyExistsException;
+import com.example.customerservice.core.exception.bank.BankAccountNotBelongToCustomerException;
+import com.example.customerservice.core.exception.bank.BankAccountNotFoundException;
+import com.example.customerservice.core.exception.bank.BankIdMismatchException;
 import com.example.customerservice.core.exception.customer.CustomerNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,29 +19,63 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(AddressNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(AddressNotFoundException ex, WebRequest request) {
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleCustomerNotFoundException(CustomerNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BankAccountAlreadyExistsException.class)
+    public ResponseEntity<ErrorDetails> handleBankAccountAlreadyExistsException(BankAccountAlreadyExistsException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BankAccountNotBelongToCustomerException.class)
+    public ResponseEntity<ErrorDetails> handleBankAccountNotBelongToCustomerException(BankAccountNotBelongToCustomerException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BankAccountNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleBankAccountNotFoundException(BankAccountNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BankIdMismatchException.class)
+    public ResponseEntity<ErrorDetails> handleBankIdMismatchException(BankIdMismatchException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AddressAlreadyExistsException.class)
+    public ResponseEntity<ErrorDetails> handleAddressAlreadyExistsException(AddressAlreadyExistsException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AddressIdMismatchException.class)
-    public ResponseEntity<?> handleAddressIdMismatchException(AddressIdMismatchException ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> handleAddressIdMismatchException(AddressIdMismatchException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<?> handleCustomerNotFoundException(CustomerNotFoundException ex, WebRequest request) {
+    @ExceptionHandler(AddressNotBelongToCustomerException.class)
+    public ResponseEntity<ErrorDetails> handleAddressNotBelongToCustomerException(AddressNotBelongToCustomerException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    // Handle global exception
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(AddressNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleAddressNotFoundException(AddressNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 }
-
