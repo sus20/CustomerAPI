2d49c28ee43f1c83734492252a412f9e02991838
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-10 13:29:35 +0200

    Add validation to createCustomer and updateCustomer endpoint
    - Added @Valid annotation to the @RequestBody parameter in the createCustomer method.
    - Ensures that incoming CustomerDTO objects are validated according to the constraints defined in the CustomerDTO class.

--- src/main/java/com/example/customerservice/adapter/in/web/CustomerController.java
+++ src/main/java/com/example/customerservice/adapter/in/web/CustomerController.java
@@ -4,6 +4,7 @@ import com.example.customerservice.adapter.in.web.dto.CustomerDTO;
 import com.example.customerservice.adapter.in.web.mapper.DTOMapper;
 import com.example.customerservice.core.model.Customer;
 import com.example.customerservice.core.port.input.ICustomerInputPort;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class CustomerController {
     private final ICustomerInputPort customerInputPort;
 
     @PostMapping
-    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
+    public ResponseEntity<CustomerDTO> createCustomer( @Valid @RequestBody CustomerDTO customerDTO) {
         Customer savedCustomer = customerInputPort.saveCustomer(DTOMapper.INSTANCE.toCustomer(customerDTO));
         return new ResponseEntity<CustomerDTO>(DTOMapper.INSTANCE.toCustomerDTO(savedCustomer), HttpStatus.CREATED);
     }
@@ -45,7 +46,7 @@ public class CustomerController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable String id, @RequestBody CustomerDTO customerDTO) {
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable String id, @Valid @RequestBody CustomerDTO customerDTO) {
         Customer customer = customerInputPort.updateCustomer(id, DTOMapper.INSTANCE.toCustomer(customerDTO));
         return ObjectUtils.isEmpty(customer)
                                             ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
@@ -56,10 +57,5 @@ public class CustomerController {
     public ResponseEntity<Void> deleteCustomer(@PathVariable String id) {
         customerInputPort.deleteCustomer(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @GetMapping("/check/{id}")
-    public boolean isRegistered(@PathVariable String id) {
-        return customerInputPort.isCustomerRegistered(id);
     }
 }
